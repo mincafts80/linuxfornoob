28be9b7ccb7f276fd95480b704a7357efb1f5db5
mincafts80 <125294556+mincafts80@users.noreply.github.com>
2023-09-16 00:22:26 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,8 @@ echo "
               H     H EEEEE IIIIII IIIIII MMMMMMMMMM
                                                      "
 cp ~/linuxfornoob/pacman.conf /etc/pacman.conf
-sudo pacman -Syu jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src discord
+sudo pacman -Syu jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src discord gimp audiotube 
+sudo pacman -Rns firefox
 git clone https://aur.archlinux.org/minecraft-launcher.git && cd ~/linuxfornoob/minecraft-launcher/ && makepkg -si && cd ~/
 git clone https://aur.archlinux.org/google-chrome.git && cd ~/linuxfornoob/google-chrome/ && makepkg -si && cd ~/
 sudo mv ~/git/pacman.conf /etc/
-
-
